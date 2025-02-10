17fb69ada325ae39f6a07a0ab97123d8ada772fc
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-10 01:48:49 +0100

    Do reverse a string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+	const splitString = string.split("");
+	let reverseString = "";
 
+	for (let index = splitString.length - 1; index >= 0; index--) {
+		reverseString += splitString[index];
+	}
+
+	return reverseString;
 };
 
 // Do not edit below this line
