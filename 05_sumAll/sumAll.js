d8a11a0813be07cde7dc4277e53b0c91d7d3cc50
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-10 02:56:08 +0100

    Do sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (arg1, arg2) {
+	let start, end;
 
+	if (
+		arg1 < 0 ||
+		arg2 < 0 ||
+		arg1 % 1 !== 0 ||
+		arg2 % 1 !== 0 ||
+		typeof arg1 !== "number" ||
+		typeof arg2 !== "number"
+	) {
+		return "ERROR";
+	}
+
+	if (arg1 > arg2) {
+		end = arg1;
+		start = arg2;
+	} else {
+		start = arg1;
+		end = arg2;
+	}
+
+	let sum = 0;
+
+	for (let i = start; i <= end; i++) {
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
