2d6390b2cb15681c5d3b6b18a001fe8c4a9c2d93
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-10 02:29:30 +0100

    Do remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, value) {
+	args = [...arguments];
+	return args[0].filter((x) => !args.slice(1).includes(x));
 };
 
 // Do not edit below this line
