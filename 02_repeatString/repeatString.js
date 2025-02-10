03a7502889af786aaca96dd4cc0402235b3c65a9
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-10 01:35:31 +0100

    Do repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+	let repeatStr = "";
+	for (let index = 0; index < num; index++) {
+		repeatStr += str;
+	}
 
+	return repeatStr;
 };
 
 // Do not edit below this line
