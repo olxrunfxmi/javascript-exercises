294356c830f4a658e91ae7aa8081b4548efdeb58
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-10 03:33:45 +0100

    Do temperature conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+	celsius = (5 / 9) * (fahrenheit - 32);
+	celsius = celsius.toFixed(1);
+	celsius = Number(celsius);
+	return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+	fahrenheit = (9 / 5) * celsius + 32;
+	fahrenheit = fahrenheit.toFixed(1);
+	fahrenheit = Number(fahrenheit);
+	return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
