e05263147392c9fd7c62d17db849f76988466e4b
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-25 23:13:13 +0100

    Do 11 getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+	return books.reduce((allBooks, book) => {
+		allBooks.push(book.title);
+		return allBooks;
+	}, []);
 };
 
 // Do not edit below this line
