2c0ac7c39f8d1a13c0014ebdde3c16468d9dbfd3
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-26 00:09:40 +0100

    Do find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	const sortedPeople = people.sort((a, b) => {
+		// Person A
+		aBirthYear = a.yearOfBirth;
+		aDeathYear = a.yearOfDeath ? a.yearOfDeath : new Date().getFullYear();
+		aAge = aDeathYear - aBirthYear;
 
+		// Person B
+		bBirthYear = b.yearOfBirth;
+		bDeathYear = b.yearOfDeath ? b.yearOfDeath : new Date().getFullYear();
+		bAge = bDeathYear - bBirthYear;
+
+		return aAge - bAge;
+    });
+    return sortedPeople[sortedPeople.length - 1]
 };
 
 // Do not edit below this line
