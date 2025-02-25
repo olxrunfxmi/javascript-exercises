7413691d5de8bfcd6345dc69aea6268964479fec
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-25 22:44:16 +0100

    Do 09 palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	// Split the string to filter out the punctuations and spaces
+	let wordSplit = str.split("");
+	let filteredWordSplit = wordSplit.filter((char) => {
+		if (!(char === "!" || char === " " || char === "," || char === ".")) {
+			return true;
+		}
+    });
+    
+	// Now, we have the filtered word in lower case (correct and reverse) for easy comparison
+	let filteredWord = filteredWordSplit.join("").toLowerCase();
+	let reversedWord = filteredWordSplit.reverse().join("").toLowerCase();
 
+	// Now, let's compare and return
+	return filteredWord === reversedWord;
 };
 
 // Do not edit below this line
