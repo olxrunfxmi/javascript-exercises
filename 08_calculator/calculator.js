da80f3ff02b4ba53f18f90c896bcc9e3c3c1c262
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-25 22:09:28 +0100

    Do 08 calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((acc, number) => acc + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((acc, number) => acc * number, 1);
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+	let series = num;
+	for (let index = 1; index < pow; index++) {
+		num *= series;
+	}
+	return num;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
